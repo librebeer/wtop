993f914b354fe84292b6f164e4f5879d9936099f
Gabriel Muñoz <blackzen666@gmail.com>
2016-10-23 13:47:38 -0500

    show RAM memory use (test)

--- test/child.js
+++ test/child.js
@@ -7,10 +7,16 @@ var max = process.argv[2];
 var n = 1;
 
 var repeat = setInterval(() => {
-  (n < max)? n++ : exit();ls
+  (n < max)? n++ : exit();
   //console.log('I want beer');
 },1000);
 
 var exit = function() {
   clearInterval(repeat);
 };
+
+process.on('message', function(msg){
+    if(msg === 'get_mem_usage'){
+        process.send({memUsage: process.memoryUsage()});
+    }
+});
--- test/testChildProcess.js
+++ test/testChildProcess.js
@@ -13,14 +13,32 @@ var timeOfLife = 60;
 // create child that does nothing
 var child = fork(__dirname + '/child.js' , [timeOfLife]);
 
+child.on('message', function(payload){
+    console.log(payload.memUsage);
+    payload.memUsage.rss
+    var mem = payload.memUsage.rss;
+    console.log(mem + " Bits");
+    console.log(mem/1024 + " Bytes");
+    console.log((mem/1024)/1024 + " MB");
+});
+
+
+
 console.log('wating a second for child creation.');
 
 setTimeout( () => {
+  //pedimos el uso de memoria.
+  child.send('get_mem_usage');
+
   console.log('Parent PID', process.pid);
   console.log('child PID:', child.pid);
+  //console.log(child.memoryUsage());
+  //console.log(process.memoryUsage());  
   console.log('life time of child:',timeOfLife,'s');
   console.log('please verify with htop!');
 },1000);
 
 // forever
 setInterval(() => {}, 2000);
+
+
